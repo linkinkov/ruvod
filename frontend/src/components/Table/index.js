e314a76b3c1d4d1780b6c11863a9fd5cbe54f6f0
linkinkov <me@linkinkov.ru>
2020-06-16 22:20:40 +0300

    createUser on client

--- frontend/src/components/Table/index.js
+++ frontend/src/components/Table/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import MaterialTable from 'material-table';
 import Alert from '@material-ui/lab/Alert';
 
@@ -14,8 +14,32 @@ const USERS = gql`
   }
 `;
 
+const ADD_USER = gql`
+  mutation createUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+      id
+      name
+      email
+    }
+  }
+`;
+
 const Table = () => {
   const { loading, error, data } = useQuery(USERS);
+  const [addUser] = useMutation(ADD_USER, {
+    update(cache, { data: { createUser } }) {
+      const { users } = cache.readQuery({ query: USERS });
+      cache.writeQuery({
+        query: USERS,
+        data: { users: users.concat([createUser]) },
+      });
+    },
+  });
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers(data?.users);
+  }, [data]);
   // TODO добавить мутации и не забыть разделить handler, а то каша
 
   return (
@@ -31,13 +55,25 @@ const Table = () => {
           field: 'email',
         },
       ]}
-      data={data?.users}
+      data={users}
       isLoading={loading}
       editable={{
         onRowAdd: newData =>
           new Promise((resolve, reject) => {
-            console.log(newData);
-            resolve();
+            addUser({
+              variables: {
+                input: {
+                  ...newData,
+                },
+              },
+            })
+              .then(() => {
+                resolve();
+              })
+              .catch(err => {
+                console.log(err);
+                reject();
+              });
           }),
         onRowUpdate: (newData, oldData) =>
           new Promise((resolve, reject) => {
